01521f58afbacec349d6fe801a2fc250b005942d
Charlie Lee <lichncjx@hotmail.com>
2023-09-01 22:19:05 +0800

    move to backend folder

--- backend/src/test/java/com/onecool/fullstack/customer/JpaCustomerRepositoryTest.java
+++ backend/src/test/java/com/onecool/fullstack/customer/JpaCustomerRepositoryTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class JpaCustomerRepositoryTest extends TestContainersUnitTestBase {
 
